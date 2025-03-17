d077e435973c0bf0d898d0df67a2c8eb29fe888f
Nataly Baranova <borashek@inbox.ru>
2025-03-17 17:38:04 +0300

    docker set up

--- index.php
+++ index.php
@@ -1,6 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Native PHP</title>
+</head>
+<body>
+<form action="index.php" method="POST">
+    <label for="quantity">Quantity</label><br/>
+    <input type="text" value="" name="quantity" id="quantity" /><br/>
+
+    <input type="submit" value="Total" />
+</form>
+</body>
+</html>
+
 <?php
-$name = 'Natasha';
-echo "Hello {$name}";
 
+$item = 'pizza';
+$price = 100.33;
+$total = null;
 
-echo '<h1>' . PHP_VERSION . '</h1>';
+if (isset($_POST['quantity'])) {
+    $quantity = $_POST["quantity"];
+    $total = $price * $quantity;
+}
+
+echo 'Your Total is: ' . $total;
+
+?>
